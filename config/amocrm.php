28c1a6abf7f7cac53aa8c046820f0f806d724bed
Artem M <artem@molotov.work>
2020-09-10 05:05:23 +0300

    Update config/amocrm.php

--- config/amocrm.php
+++ config/amocrm.php
@@ -17,16 +17,29 @@ return [
     |--------------------------------------------------------------------------
     |
     | Эти параметры необходимы для авторизации в системе amoCRM.
-    | - Поддомен компании. Приставка в домене перед .amocrm.ru;
-    | - Логин пользователя. В качестве логина в системе используется e-mail;
-    | - Ключ пользователя, который можно получить на странице редактирования
-    |   профиля пользователя.
-    |
+    | - Домен компании (включая .amocrm.ru). Например, example.amocrm.ru;
+    | - Параметр "состояния". "Сгенерированный вами строковый параметр, возможно хеш.
+    |   State нужен для того, чтобы при получении ответа от amoCRM, вы могли проверить его достоверность,
+    |   сравнив отправленный ключ и полученный в результате, чтобы удостовериться в отсутствии подмены CSRF
+    | - ID интеграции.
+    | - Секрет интеграции. Получить можно следуя инструкцие на странице
+    | - Код авторизации (действителен 20 минут). Необходим единоразово для первоначальной настройки
+    | - Redirect URI указанный в настройках интеграции
+    |   https://www.amocrm.ru/developers/content/oauth/easy-auth
+    |   https://www.amocrm.ru/developers/content/oauth/step-by-step#request-authorization-code
+    |   Архив:
+    |   https://web.archive.org/web/20191030063225/https://www.amocrm.ru/developers/content/oauth/easy-auth
+    |   https://web.archive.org/web/20191030064552/https://www.amocrm.ru/developers/content/oauth/step-by-step#get_auth_code
     */
 
-    'domain' => env('AMO_DOMAIN', 'example'),
-    'login' => env('AMO_LOGIN', 'login@example.com'),
-    'hash' => env('AMO_HASH', 'd56b699830e77ba53855679cb1d252da'),
+    'domain' => env('AMO_DOMAIN', 'example.amocrm.ru'),
+    'state' => env('AMO_STATE', (string) rand(10000,PHP_INT_MAX)),
+    'clientId' => env('AMO_CLIENT_ID', 'TODO CHANGE'),
+    'clientSecret' => env('AMO_CLIENT_SECRET', 'TODO CHANGE'),
+    'clientAuthorizationCode' => env('AMO_CLIENT_AUTHORIZATION_CODE', 'TODO CHANGE'),
+    'redirectUri' => env('AMO_REDIRECT_URI', 'about:blank?'),
+
+    'tokensFilePath' => storage_path('vendor/dotzero/laravel-amocrm/tokens.dat'),
 
     /*
     |--------------------------------------------------------------------------
